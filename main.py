e783d3d833e954348f54a3a02ac926a406c76c8b
Brittany Chan <brittanyhaileec@gmail.com>
2024-11-30 09:54:38 -0800

    trialed gemini api--it works; added gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from flask import Flask
+import google.generativeai as genai
+
+
+filename = 'apikey'
+
+def get_file_contents(filename):
+    try: 
+        with open (filename, 'r') as f:
+            return f.read().strip()
+
+    except FileNotFoundError:
+        print("'%s' file not found" % filename)
+
+
+genai.configure(api_key=get_file_contents(filename))
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Explain how AI works")
+print(response.text)
+
 
 app = Flask(__name__)
 
